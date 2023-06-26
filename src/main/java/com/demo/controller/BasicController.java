dfbae836c4ee6145638f4735ffb3996d31246576
sac2277 <sachinnlohar@gmail.com>
2023-06-27 05:20:02 +0530

    first commit

---
+++ src/main/java/com/demo/controller/BasicController.java
@@ -0,0 +1,14 @@
+package com.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BasicController {
+
+	@GetMapping("/get")
+	public String getData() {
+		return "WelcomeToLoveToLearn";
+	}
+}
+
